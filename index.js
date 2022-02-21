e3b06a7db52557057795e397475e261f98242d1b
ishakcoban <ishakcoban99@outlook.com>
2022-02-21 13:48:59 +0300

    cors policy added and port changed to 4000

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser=require('body-parser')
+const cors = require('cors');
 require('./db/dbConnection');
 const app = express();
 
 //Middlewares
+app.use(cors)
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -18,6 +20,6 @@ app.get('/',(req,res) => {
 res.json({'mesaj':'açıldı'});
 });
 
-app.listen(3000,() =>  {
+app.listen(4000,() =>  {
 console.log("çalıştı")
 });
